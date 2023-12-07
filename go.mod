46a5d22d73d2e9dc7eefd2409044a9dc82f50544
Alfred-Jijo <alfredjijo28@gmail.com>
2023-12-07 20:33:25 +0000

    idk anymore

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/Alfred-Jijo/todoapp-ClI
+
+go 1.21.5
+
+require github.com/gonuts/commander v0.4.1
+
+require (
+	github.com/gonuts/flag v0.1.0
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/posener/complete v1.2.3 // indirect
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/Alfred-Jijo/todoapp-ClI/cmd"
+	"github.com/gonuts/commander"
+)
+
+const (
+	todoFilename = ".todo"
+)
+
+func main() {
+	filename := ""
+	existCurTodo := false
+	curDir, err := os.Getwd()
+	if err == nil {
+		filename = filepath.Join(curDir, todoFilename)
+		_, err = os.Stat(filename)
+		if err == nil {
+			existCurTodo = true
+		}
+	}
+	if !existCurTodo {
+		home := os.Getenv("HOME")
+		if home == "" {
+			home = os.Getenv("USERPROFILE")
+		}
+		filename = filepath.Join(home, todoFilename)
+	}
+	command := &commander.Command{
+		UsageLine: os.Args[0],
+		Short:     "todo for cli",
+	}
+	command.Subcommands = []*commander.Command{
+		cmd.MakeCmdAdd()
+	}
+	err = command.Dispatch(os.Args[1:])
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+}
